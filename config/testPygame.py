aaac5df63b96652192f0c363d2a0d24849853734
Florent <grenaille.florent85@gmail.com>
2019-01-21 19:06:03 +0100

    test

---
+++ config/testPygame.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+import pygame
+
+import os
+
+file_name = "floor-tiles-20x20.png"
+#path_to_file = os.path.join("../ressource", file_name)
+
+pygame.init()
+pygame.font.init()
+
+fenetre = pygame.display.set_mode((640, 480), pygame.RESIZABLE)
+
+fond = pygame.image.load(file_name).convert()
+
+fenetre.blit(fond, (0, 0))
+pygame.display.flip()
+
+continuer = True
+
+while continuer:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            continuer = False
